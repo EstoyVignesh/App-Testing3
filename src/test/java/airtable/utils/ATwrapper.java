aa7c2cd99a26cb73b9d289ce27ae546621b2970f
Vignesh Palanisamy <143159020+EstoyVignesh@users.noreply.github.com>
2024-12-11 10:59:08 +0530

    Update ATwrapper.java

--- src/test/java/airtable/utils/ATwrapper.java
+++ src/test/java/airtable/utils/ATwrapper.java
@@ -10,7 +10,7 @@ public class ATwrapper extends SeWrapper{
 	public void loginandRunapp1() throws Throwable
 	{
 	
-	Airtable1 at = PageFactory.initElements(driver,Airtable1.class);
+	Airtable01 at = PageFactory.initElements(driver,Airtable01.class);
 	
 	at.email("Konnectifyqa+5@gmail.com");
 	at.Password("Testing@123");
@@ -23,7 +23,78 @@ public class ATwrapper extends SeWrapper{
 	at.selectConnection();
 	at.selectTrigger();
 	at.clickContinuebutton1();
+	
+	at.clickBase1();
+	
+	
+	at.clickTable1();
+	
 	at.clickContinuebutton2();
+	
+	at.searchApp1("Airtable");
+	at.selectApp1();
+	at.selectConnection1();
+	at.selectAction();
+	at.clickContinuebutton3();
+	
+	at.clickBase2();
+	
+	
+	at.clickTable2();
+	at.nameField("name");
+	at.clickContinuebutton4();
+	at.clickSaveButton();
+	at.nameKonnector("Airtable-NewRecord$CreateRecord");
+	at.clickSaveChanges();
+	at.clickPublish();
+	at.clickActiveNow();
+	at.closeAllBrowser();
+	
+		
+	}
+	
+	public void loginandRunapp2() throws Throwable
+	{
+	
+	Airtable02 at = PageFactory.initElements(driver,Airtable02.class);
+	
+	at.email("Konnectifyqa+5@gmail.com");
+	at.Password("Testing@123");
+	at.Login();
+	at.ButtonNew();
+	at.Konnector();
+	at.trigger();
+	at.searchApp("Airtable");
+	at.selectApp();
+	at.selectConnection();
+	at.selectTrigger();
+	at.clickContinuebutton1();
+	
+	at.clickBase1();
+	
+	
+	at.clickTable1();
+	at.selectModified();
+	
+	at.clickContinuebutton2();
+	
+	at.searchApp1("Airtable");
+	at.selectApp1();
+	at.selectConnection1();
+	at.selectAction();
+	at.clickContinuebutton3();
+	
+	at.clickBase2();
+	at.clickTable2();
+	at.clickRecordID();
+	at.clickUdateMethod(1000);
+	at.nameField("name");
+	at.clickContinuebutton4();
+	at.clickSaveButton();
+	at.nameKonnector("Airtable-UpdatedRecord$UpdateRecord");
+	at.clickSaveChanges();
+	at.clickPublish();
+	at.clickActiveNow();
 	
 		
 	}
